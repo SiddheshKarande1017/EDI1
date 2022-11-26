ca0e531013d305c7d857a56490f2e11751f0c7ff
unknown <siddheshkarande1017@gmail.com>
2022-11-26 21:50:46 +0530

    files add

---
+++ djangochat/chat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.register, name='register'),
+    path('login/', views.loginuser, name="login"),
+    path('login/home', views.home, name='home'),
+    path('login/problems',views.problems, name='problems'),
+    path('logout/', views.logoutuser, name="logout"),
+
+]
---
+++ djangochat/chat/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm
+from django.contrib import messages
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def register(request):
+    form_pre = UserForm()
+    if request.method == 'POST':
+        form_pre = UserForm(request.POST)
+        if form_pre.is_valid():
+            form_pre.save()
+            messages.success(request, 'Account created successfully..')
+            return redirect('login')
+    context = {'form': form_pre}
+    return render(request,"register.html",context)
+
+#--Login view-->
+def loginuser(request):
+    if request.method == 'POST':
+
+        username_entered = request.POST.get('username')
+        password_entered = request.POST.get('password')
+
+        user = authenticate(request,username = username_entered, password = password_entered)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,"username or password is incorrect...")
+    context = {}
+    return render(request,"login.html",context)
+
+def problems(request):
+    return render(request,"problem.html")
+
+def logoutuser(request):
+    logout(request)
+    return redirect('register')
